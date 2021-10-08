d1cf7816306db30868254723aaea58e419bb2eb6
ines ribeiro <ines.ribeiro@student.ie.edu>
2021-10-08 15:06:58 +0200

    changes made

--- session2.py
+++ session2.py
@@ -1,7 +1,6 @@
 session2.py
 
 def func(a, b):
-<<<<<<< HEAD
     return "hello"
 
 def func2(a, b):
@@ -9,10 +8,3 @@ def func2(a, b):
   
 
     
-=======
-    return "goodbye"
-
-def func2(a, b):
-    return a * b
-    
->>>>>>> 15822990751c27b08dd7b005f3a67e8ab6579275
